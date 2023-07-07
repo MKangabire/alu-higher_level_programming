ca86d6d82ee3cd4746a1e26aff885cd5d05b4d38
MKangabire <m.kangabire@alueducation.com>
2023-07-07 04:20:10 -0700

    pos or ned

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+def num(x):
+    if (x<0):
+        print("is positive")
+    elif(x == 0):
+        print("is zero")
+    else:
+        print("is negative")
