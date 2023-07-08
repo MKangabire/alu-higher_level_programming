1487dc576b923325a500db113f0a2da6c4f41132
MKangabire <m.kangabire@alueducation.com>
2023-07-08 10:30:36 -0700

    last digit

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz(x):
+    for num in range(1, x+1):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif num % 3 == 0:
+            print("Fizz", end=" ")
+        elif num % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(num, end=" ")
