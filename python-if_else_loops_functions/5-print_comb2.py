68672ca581b21748d5e4e576b941aa6538a38e83
MKangabire <m.kangabire@alueducation.com>
2023-07-08 02:16:21 -0700

    comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f"{str(i).zfill(2)}, ", end="")
-print("99")
+    if i < 99:
+        print("{:02}, ".format(i), end= "")
+    else:
+        print("{:02}".format(i))
