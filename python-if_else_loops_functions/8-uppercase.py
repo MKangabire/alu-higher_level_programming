0fce63cb6f8a5b756a1640845b709b5390537411
MKangabire <m.kangabire@alueducation.com>
2023-07-08 09:02:51 -0700

    last digit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def uppercase(c):
-    return ord(c[0]) >= 65 and ord(c[0]) <= 90 
+def uppercase(string):
+    upper_string = ""
+    for char in string:
+        if ord(char) >= 97 and ord(char) <= 122:
+            upper_string += "{}".format(chr(ord(char) - 32))
+        else:
+            upper_string += "{}".format(char)
+    print("{}\n".format(upper_string))
