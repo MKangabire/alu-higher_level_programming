5323218b5d8dc485103b39a8c130d24a8c397c02
MKangabire <m.kangabire@alueducation.com>
2023-07-09 08:38:59 -0700

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import sys
+args = sys.argv[1:]
+num_args = len(args)
 
-if __name__ == "__main__":
-    argv = sys.argv[1:]
-    argc = len(argv)
-
-    print("{} argument{}:".format(argc, "s" if argc != 1 else ""))
-    if argc == 0:
-        print("")
-    else:
-        for i, arg in enumerate(argv, 1):
-            print("{}: {}".format(i, arg))
+if num_args == 0:
+    print("0 arguments.")
+else:
+    print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+    for i, arg in enumerate(args):
+        print(f"{i+1}: {arg}")
