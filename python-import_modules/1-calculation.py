c71dfdf67737839a7f8393548799432f116e4e3b
MKangabire <m.kangabire@alueducation.com>
2023-07-09 04:39:28 -0700

    add

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,9 +6,9 @@ def main():
     a = 10
     b = 5
     result = add(a, b)
-    result2 = sub(a, b)
-    result3 = mul(a, b)
-    result4 = div(a, b)
+    result1 = sub(a, b)
+    result2 = mul(a, b)
+    result3 = div(a, b)
     print("{} + {} = {}".format(a, b, result))
     print("{} - {} = {}".format(a, b, result1))
     print("{} * {} = {}".format(a, b, result2))
