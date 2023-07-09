802dc33be4dc55bbed84d28db04d1670d1d5451e
MKangabire <m.kangabire@alueducation.com>
2023-07-09 08:50:29 -0700

    args

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import types
+    import dis
+    import importlib.util
 
-import types
-import dis
-import importlib.util
 
+    spec = importlib.util.spec_from_file_location("hidden_4", "/path/to/hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 
-spec = importlib.util.spec_from_file_location("hidden_4", "/path/to/hidden_4.pyc")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
+    names = dir(module)
 
-names = dir(module)
+    names = [name for name in names if not name.startswith("__")]
 
-names = [name for name in names if not name.startswith("__")]
+    names.sort()
 
-names.sort()
-
-for name in names:
-    print(name)
-```
-
+    for name in names:
+        print(name)
