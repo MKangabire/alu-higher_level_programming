abf80da5fcac09feaab1e840e5233a58aa7fe2c3
MKangabire <m.kangabire@alueducation.com>
2023-07-09 07:33:46 -0700

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    print("{} argument{}:".format(argc, "s" if argc != 1 else "")
+    if argc == 0:
+        print(".")
+     else:
+        for i, arg in enumerate(argv, 1):
+            print("{}: {}".format(i, arg))
