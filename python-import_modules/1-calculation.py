6bac5946166262230827c946d41e0d2554af9bd6
MKangabire <m.kangabire@alueducation.com>
2023-07-09 04:27:46 -0700

    add

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 from calculator_1 import *
-a = 10
-b = 5
-result = add(a, b)
-result1 = sub(a, b)
-result2 = mul(a, b)
-result3 = div(a, b)
-print("{} + {} = {}".format(a, b, result))
-print("{} - {} = {}".format(a, b, result1))
-print("{} * {} = {}".format(a, b, result2))
-print("{} / {} = {}".format(a, b, result3))
+def main():
+    a = 10
+    b = 5
+    result = add(a, b)
+    result1 = sub(a, b)
+    result2 = mul(a, b)
+    result3 = div(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    print("{} - {} = {}".format(a, b, result1))
+    print("{} * {} = {}".format(a, b, result2))
+    print("{} / {} = {}".format(a, b, result3))
+if __name__ = "__main__":
+    main()
