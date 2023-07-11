2aefdf8b06b8eea20223bbb35517608837de39b3
MKangabire <m.kangabire@alueducation.com>
2023-07-11 02:23:49 -0700

    hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 import dis
-import marshal
 import types
 
-def print_names():
-    with open('hidden_4.pyc', 'rb') as file:
-        magic = file.read(4)
-        timestamp = file.read(4)
-        code_object = marshal.load(file)
+def get_names():
+    with open('hidden_4.pyc', 'rb') as f:
+        code = f.read()
+    module = types.ModuleType('hidden_4')
+    module.__file__ = 'hidden_4.pyc'
+    module.__loader__ = None
+    module.__package__ = ''
+    exec(compile(code, 'hidden_4.pyc', 'exec'), module.__dict__)
+    return [name for name in dir(module) if not name.startswith('__')]
 
-    instructions = dis.Bytecode(code_object)
-    names = set()
-
-    for instruction in instructions:
-        if instruction.opname == 'LOAD_NAME':
-            name = instruction.argval
-            if not name.startswith('__'):
-                names.add(name)
-
-    for name in sorted(names):
-        print(name)
-
+for name in sorted(get_names()):
+    print(name)
 if __name__ == '__main__':
     print_names()
