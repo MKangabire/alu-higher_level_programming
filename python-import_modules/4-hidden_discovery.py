3b3a06ace78e7b9fe0d9081f5d6813b5ffcfff58
MKangabire <m.kangabire@alueducation.com>
2023-07-11 07:12:52 -0700

    higher

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ import hidden_4
 def main():
     for content in dir(hidden_4):
         if content[:2] != '__':
-        print(f"{:s}".format(content))
+            print(f"{:s}".format(content))
 
         
 if __name__ == '__main__':
