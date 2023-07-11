9aa99f21ceb669397bd3ffc9e812f621ec1e4aec
MKangabire <m.kangabire@alueducation.com>
2023-07-11 01:46:12 -0700

    hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import dis
+import marshal
 import types
 
 def print_names():
     with open('hidden_4.pyc', 'rb') as file:
         magic = file.read(4)
         timestamp = file.read(4)
-        code_object = types.CodeType.from_bytes(file.read())
+        code_object = marshal.load(file)
 
     instructions = dis.Bytecode(code_object)
     names = set()
