5b2039790f266520fa497c1702296ae8deeb4b14
MKangabire <m.kangabire@alueducation.com>
2023-07-11 07:06:03 -0700

    higher

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-import dis
-import types
+import hidden_4
 
-def get_names():
-    with open('hidden_4.pyc', 'rb') as f:
-        code = f.read()
-    module = types.ModuleType('hidden_4')
-    module.__file__ = 'hidden_4.pyc'
-    module.__loader__ = None
-    module.__package__ = ''
-    exec(compile(code, 'hidden_4.pyc', 'exec'), module.__dict__)
-    return [name for name in dir(module) if not name.startswith('__')]
+def main():
+    for content in dir(hidden_4):
+        if content[:2] != '_'
+        print(f"{:s}".format(content))
 
-for name in sorted(get_names()):
-    print(name)
+        
 if __name__ == '__main__':
     print_names()
