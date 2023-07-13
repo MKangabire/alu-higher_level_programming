7458391cd4b78ead72b74ce21c82fb1dcb0d5a32
MKangabire <m.kangabire@alueducation.com>
2023-07-13 06:50:31 -0700

    integer

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2] + (0, 0)
+    a1, a2 = tuple_a[:2] + (0, 0)[:2]
 
-    b1, b2 = tuple_b[:2] + (0, 0)
+    b1, b2 = tuple_b[:2] + (0, 0)[:2]
 
     return (a1 + b1, a2 + b2)
-
