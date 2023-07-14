30c03704ed3209a715bf8df90bd2da75f9380ee1
MKangabire <m.kangabire@alueducation.com>
2023-07-14 11:03:31 -0700

    list

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,6 @@ def multiple_returns(sentence):
     first_letter = sentence[0]
     length = len(sentence)
     if len(sentence) == 0:
-        if first_letter is None:
-            return (int(length), first_letter)
+        return (int(length), first_letter)
     else:
        return int(length), first_letter
