90add4185309e0d6ec18a6cfbd42a419029c0507
MKangabire <m.kangabire@alueducation.com>
2023-07-15 03:59:16 -0700

    divisible

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
     reversed_list = list(reversed(my_list))
     if not my_list:
         return
     else:
         for i in reversed_list:
-            print("{:d}".format(i))
+           i print("{:d}".format(i))
