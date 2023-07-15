e6662a5159752170add2de08222be263c35a0f89
MKangabire <m.kangabire@alueducation.com>
2023-07-15 03:17:20 -0700

    divisible

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    result = []
     for i in my_list:
-        if i % 2:
-            return True
-        else:
-            return False
+        result.append(i % 2 == 0)
+    return result
