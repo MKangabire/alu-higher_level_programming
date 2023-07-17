39b538c8f930e298e90f3bc246e01e641d1c0e03
MKangabire <m.kangabire@alueducation.com>
2023-07-16 23:14:28 -0700

    matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,9 @@
 def square_matrix_simple(matrix=[]):
     new_matrix=[]
     for sublist in matrix:
-        new_list = (element**2 for element in sublist)
+        new_list=[]
+        for element in sublist:
+            new_element = element**2
+            new_list.append(new_element)
         new_matrix.append(new_list)
     return new_matrix
