ec655f1a292b2279c75e78d9b116c500326f2660
MKangabire <m.kangabire@alueducation.com>
2023-07-17 00:41:30 -0700

    matrix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
     total = 0
     prev_value = 0
 
     for char in reversed(roman_string):
-        value = roman_dict[char]
+        value = roman_dict.get(char)
+        if value is None:
+            return 0
         if value >= prev_value:
             total += value
         else:
