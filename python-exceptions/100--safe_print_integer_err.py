d40a4966205ad837c13ec370d095e4b2736eecc5
MKangabire <m.kangabire@alueducation.com>
2023-07-17 23:58:15 -0700

    tuple

---
+++ python-exceptions/100--safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value, end="\n"))
+        return True
+    except Exception as err_message:
+        print("Exception: {}".format(err_message), file=sys.stderr)
+        return False
