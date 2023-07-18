fd064f3e660cce048c185b159780489a2a029ef7
MKangabire <m.kangabire@alueducation.com>
2023-07-17 23:36:17 -0700

    tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2] + (0, 0)[:2]
-
-    b1, b2 = tuple_b[:2] + (0, 0)[:2]
-
-    return (a1 + b1, a2 + b2)
+    length_a = len(tuple_a)
+    length_b = len(tuple_b)
+    if length_a == 0 or length_b == 0:
+        if length_a == 0 and length_b == 0:
+            tuple_a = (0, 0)
+            tuple_b = (0, 0)
+        elif length_a == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_b = (0, 0)
+    elif length_a == 1 or length_b == 1:
+        if length_a == 1 and length_b == 1:
+            tuple_a = (tuple_a[0], 0)
+            tuple_b = (tuple_b[0], 0)
+        elif length_a == 1:
+            tuple_a = (tuple_a[0], 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
+    else:
+        if length_a > 2 and length_b > 2:
+            tuple_a = (tuple_a[0], tuple_a[1])
+            tuple_b = (tuple_b[0], tuple_b[1])
+        elif length_a == 0:
+            tuple_a = (tuple_a[0], tuple_a[1])
+        else:
+            tuple_b = (tuple_b[0], tuple_b[1])
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result:{:.1f}".format(result, end=""))
     except ZeroDivisionError:
-        print("Inside result: {:.1f}".format(None))
-        return None
-    except (ValueError, TypeError):
-        return None
+        result = None
     finally:
-        print()
+        print("Inside result:{:.1f}".format(result))
