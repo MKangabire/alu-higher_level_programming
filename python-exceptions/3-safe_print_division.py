766090372c89196323454032bd18b024d4f93475
MKangabire <m.kangabire@alueducation.com>
2023-07-17 23:55:45 -0700

    tuple

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-    except(TypeError):
+    except(TypeError, ValueError):
         return None
     finally:
         print("Inside result: {:.1f}".format(result))
