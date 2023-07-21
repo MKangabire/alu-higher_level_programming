9685063cf2b533e6dcd9d33e9cdb1c54dcef9a32
MKangabire <m.kangabire@alueducation.com>
2023-07-21 01:18:41 -0700

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is an oop that mentions the size of a square
+"""
+
+
+class Square:
+    """
+    this is a class of squares
+    """
+    def __init__(self, size):
+        self.__size = size
+
