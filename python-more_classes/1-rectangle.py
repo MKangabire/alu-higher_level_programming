4e2da674f94804218b2d1aa729af45adf598d1e7
MKangabire <m.kangabire@alueducation.com>
2023-07-22 01:58:55 -0700

    rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+>>>
+"""
 class Rectangle:
+    """
+    >>>>>>>>>>>>>>>
+    """
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
