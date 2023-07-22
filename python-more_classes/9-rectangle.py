e629e49a239cf86bae354ae36462bd452dd2c66a
MKangabire <m.kangabire@alueducation.com>
2023-07-22 06:53:51 -0700

    rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
-.....................
+..........
 """
 
 
 class Rectangle:
     """
-    ......
+    ........
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -35,12 +38,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
     def area(self):
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__widthi == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__width + self.__height)
@@ -58,3 +60,17 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+    def square(cls, side=0):
+        return cls(side, side)
