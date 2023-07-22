27676677c6c47d5c9cd1695bb540a74271cc7ace
MKangabire <m.kangabire@alueducation.com>
2023-07-22 06:56:34 -0700

    rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -40,8 +40,10 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-         if self.__width == 0 or self.__height == 0 else 2 * (self.__width + self.__height)
-         return 0
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
