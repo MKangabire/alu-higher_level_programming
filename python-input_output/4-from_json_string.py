42336ccc9c3579ba515ab038d7353d51735a52b1
MKangabire <m.kangabire@alueducation.com>
2023-07-25 10:00:50 -0700

    json

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this a script where a json string is given out
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    json
+    """
+    return json.loads(my_str)
