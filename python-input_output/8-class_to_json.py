b93f08a781f7c9a78932beec4b8a779855601106
MKangabire <m.kangabire@alueducation.com>
2023-07-25 14:11:05 -0700

    add item

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a script that returns the dict description with simple data structure
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """
+    json
+    """
+    return json.dumps(obj)
