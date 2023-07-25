3875db18bc361e9e382926ddbab4caa380682cba
MKangabire <m.kangabire@alueducation.com>
2023-07-25 14:15:49 -0700

    add item

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,11 +3,27 @@
 a script that returns the dict description with simple data structure
 """
 
-import json
-
 
 def class_to_json(obj):
     """
-    json
+    Convert an instance of a Class to a dictionary representation suitable for JSON serialization.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        dict: A dictionary representation of the object suitable for JSON serialization.
     """
-    return json.dumps(obj)
+
+    json_dict = {}
+
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+        elif isinstance(value, (tuple, set)):
+            json_dict[key] = list(value)
+        elif isinstance(value, object):
+            json_dict[key] = class_to_json(value)
+
+    return json_dict
+
