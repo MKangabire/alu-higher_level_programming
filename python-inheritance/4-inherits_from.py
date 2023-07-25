c9974e131d554302200555e5c1f1995f79b108f7
MKangabire <m.kangabire@alueducation.com>
2023-07-24 23:29:59 -0700

    list

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a python script that returns true if the object is an instance of a class that is inherited """
+a python script that returns true if the object is an instance of a class that is inherited
+"""
 
 
 def inherits_from(obj, a_class):
