5eb9bc9205e41cbd49bfd5f9f748f214c43ca3e1
MKangabire <m.kangabire@alueducation.com>
2023-07-24 23:41:38 -0700

    list

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a python script that gives out the area 
+"""
+
+class BaseGeometry:
+    """
+    ...................
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
