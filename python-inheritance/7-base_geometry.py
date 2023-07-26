92ad0bc1bdb9f21ef27cbde6ce11c730fab01210
MKangabire <m.kangabire@alueducation.com>
2023-07-26 02:04:58 -0700

    triangle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,8 +11,10 @@ class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
 
-    def integer_validation(self, name, value):
+    def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
+        self.name = name
+        self.value = value
