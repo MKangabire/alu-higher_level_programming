0be2f3d8c25b787c34bd8f78d6ccad8c07818247
MKangabire <m.kangabire@alueducation.com>
2023-07-31 23:24:44 -0700

    SQL

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
 -- prints the full description of the table
 
-USE htbn_0c_0
-SHOW first_table;
+USE hbtn_0c_0;
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
