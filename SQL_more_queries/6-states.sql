9b2906707776dbadca85f99780f2e6daf3a10fd2
MKangabire <m.kangabire@alueducation.com>
2023-08-02 02:53:38 -0700

    SQL

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- a script that creates a database
+-- hbtn_0d_usa and the table states
+
+CREATE DATABASE hbtn_0d_usa
+CREATE TABLE states(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
