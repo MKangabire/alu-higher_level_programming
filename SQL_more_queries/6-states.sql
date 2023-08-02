8a6a274c9dc74ed643bcad6a3f41ef42213de140
MKangabire <m.kangabire@alueducation.com>
2023-08-02 02:57:23 -0700

    SQL

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- hbtn_0d_usa and the table states
 
 CREATE IF NOT EXISTS DATABASE hbtn_0d_usa
-CREATE TABLE states(
+CREATE IF NOT EXISTS TABLE states(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
