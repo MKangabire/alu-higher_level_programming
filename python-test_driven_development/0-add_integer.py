043e736bf685cf8baffbd12556eea3b7b9eacb06
MKangabire <m.kangabire@alueducation.com>
2023-08-07 00:30:01 -0700

    tdd

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,9 +5,12 @@ a function that adds 2 integers
 
 
 def add_integer(a, b=98):
+    """
+    adds 2 integers
+    """
     if not isinstance(a, float):
         raise TypeError("a must be an integer")
-     if not isinstance(b, float):
+    if not isinstance(b, float):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
