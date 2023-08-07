368142af61ff71f74ec5c04b3c2a7be01d8637cc
MKangabire <m.kangabire@alueducation.com>
2023-08-07 00:27:43 -0700

    tdd

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function that adds 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, float):
+        raise TypeError("a must be an integer")
+     if not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    results = sum(a, b)
+    return results
