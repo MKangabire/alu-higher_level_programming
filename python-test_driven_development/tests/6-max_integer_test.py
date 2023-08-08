11d0d919e1a5be037c7359e472daa53969a42efd
MKangabire <m.kangabire@alueducation.com>
2023-08-08 02:46:34 -0700

    TDD

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_single_element(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([2, 7, 1, 10]), 10)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-5, -2, -10]), -2)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-5, 0, 10, -3, 8]), 10)
+
+    def test_duplicate_max(self):
+        self.assertEqual(max_integer([5, 5, 5]), 5)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([3.5, 1.2, 4.8]), 4.8)
+
+if __name__ == '__main__':
+    unittest.main()
