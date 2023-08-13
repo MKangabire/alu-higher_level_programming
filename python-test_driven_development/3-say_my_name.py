1bc2bf2eb805da48f85a1bc01be3f305387079d0
MKangabire <m.kangabire@alueducation.com>
2023-08-13 02:50:52 -0700

    TDD

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,11 +8,12 @@ def say_my_name(first_name, last_name=""):
     """
     prints the name 
     """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-       raise TypeError("last_name must be a string")
-    if last_name:
-        print("My name is " + first_name + " " + last_name)
+    try:        
+        if not isinstance(first_name, str):
+            raise TypeError("first_name must be a string")
+        if not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
+    except TypeError:
+        raise
     else:
-        print("My name is " + first_name)
+         return print("My name is {} {}".format(first_name, last_name)) 
