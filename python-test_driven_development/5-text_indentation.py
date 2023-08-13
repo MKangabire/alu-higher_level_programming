ffa1b0330beb8cefbf9db25293bb86f8272b68b4
MKangabire <m.kangabire@alueducation.com>
2023-08-13 03:28:09 -0700

    TDD

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,17 +8,24 @@ def text_indentation(text):
     """
     prints a text with 2 new lines after each of these characters: ., ? and :
     """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-    separators = ['.', '?', ':']
-    sentences = []
-    current_sentence = ''
-    
-    for char in text:
-        current_sentence += char
-        if char in separators:
-            sentences.append(current_sentence.strip())
-            current_sentence = ''
-    
-    for sentence in sentences:
-        print(sentence)
+    try:
+        if not isinstance(text, str):
+            raise TypeError("text must be a string")
+        i = 0
+        new_string = ""
+        while i < len(text):
+            if text[i] in ['?', '.', ':']:
+                new_string += text[i]
+                new_string += "\n\n"
+                if i < len(text) - 1 and text[i + 1] == " ":
+                    i += 1
+                    while text[i] == " ":
+                        i += 1
+                elif i < len(text) - 1 and text[i + 1] != " ":
+                    i += 1
+            else:
+                new_string += text[i]
+                i += 1
+        return print(new_string, end="")
+    except TypeError:
+        raise
