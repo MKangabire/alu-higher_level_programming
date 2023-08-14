a16c2942fcef71738d9b56058c3b5c370d687fb0
MKangabire <m.kangabire@alueducation.com>
2023-08-14 05:13:49 -0700

    ORM

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     query = ("SELECT * FROM states "
-            "WHERE BINARY name LIKE 'N%' "
-            "ORDER BY states.id")
+             "WHERE BINARY name LIKE 'N%' "
+             "ORDER BY states.id")
     cursor.execute(query)
 
     rows = cursor.fetchall()
