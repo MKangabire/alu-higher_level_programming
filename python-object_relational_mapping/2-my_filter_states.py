8a22d6f9faa382c036eaab88ea7779cfef174be9
MKangabire <m.kangabire@alueducation.com>
2023-08-16 23:45:22 -0700

    ORM

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,37 +6,37 @@ a script that takes in an argument and displays all values in the states tabl
 import MySQLdb
 import sys
 
-    def search_states(username, password, db_name, state_name):
-        """
-        search states
-        """
-        try:
-            db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=mysql_username,
-                passwd=mysql_password,
-                db=db_name
-            )
-            cursor = db.cursor()
+def search_states(username, password, db_name, state_name):
+    """
+    search states
+    """
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name
+        )
+        cursor = db.cursor()
 
-            query = ("SELECT * FROM states "
-                     "WHERE name = '{}' "
-                     "ORDER BY states.id").format(state_name)
-            cursor.execute(query)
+        query = ("SELECT * FROM states "
+                 "WHERE name = '{}' "
+                 "ORDER BY states.id").format(state_name)
+        cursor.execute(query)
 
-            if not rows:
-                print("No matching states found.")
-            else:
-                for row in rows:
-                    print(row)
+        if not rows:
+            print("No matching states found.")
+        else:
+            for row in rows:
+                print(row)
 
-            cursor.close()
-            db.close()
+        cursor.close()
+        db.close()
 
-        except MySQLdb.Error as e:
-            print("Error:", e)
-            sys.exit(1)
+    except MySQLdb.Error as e:
+        print("Error:", e)
+        sys.exit(1)
 
 if __name__ == "__main__":
         if len(sys.argv) != 5:
