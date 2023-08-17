359872fd73b2d6029df820096b2ede7f206b263a
MKangabire <m.kangabire@alueducation.com>
2023-08-16 23:56:11 -0700

    ORM

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,43 @@
+#!/usr/bin/python3
+"""
+a script that takes in arguments and displays all values in the states table
+"""
+
 import MySQLdb
 import sys
 
+
 def search_states(username, password, db_name, state_name):
     try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(host='localhost',
+        
+        db = MySQLdb.connect(
+                host='localhost',
                 port=3306,
                 user=username,
                 passwd=password,
                 db=db_name)
 
         cursor = db.cursor()
+
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+        cursor.execute(query, (state_name,))
+
+        results = cursor.fetchall()
+
+        for row in results:
+            print(row)
+
+        cursor.close()
+        db.close()
+        
+    except MySQLdb.Error as e:
+        print("Error:", e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <mysql_username> <mysql_password>"
+              "<db_name> <state_name>")
+        sys.exit(1)
 
 
