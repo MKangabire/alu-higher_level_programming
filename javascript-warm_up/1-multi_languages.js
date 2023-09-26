f906b3c45a22bff6c375dee0aabc6cb1f51166a5
MKangabire <m.kangabire@alueducation.com>
2023-09-26 09:00:30 -0700

    Javascript

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function print(sentence) {
-	console.log(sentence);
+function print (sentence) {
+		console.log(sentence);
 }
-print("C is fun");
-print("Python is cool");
-print("JavaScript is amazing");
+print('C is fun');
+print('Python is cool');
+print('JavaScript is amazing');
