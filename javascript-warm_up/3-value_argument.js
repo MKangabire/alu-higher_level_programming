a9a9c6a25fb29dff43cc00cee89459a274685bac
MKangabire <m.kangabire@alueducation.com>
2023-09-28 05:20:47 -0700

    javascript

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function print () {
+	const args = process.argv.slice(2);
+	
+	if (args[0] === undefined) {
+	    console.log("No argument");
+	} else {
+	   console.log(args[0]);
+  }
+}
+print()
