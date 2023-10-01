59b8870d23df9a35da7f7c31ebec27e8bc4c94a8
MKangabire <m.kangabire@alueducation.com>
2023-10-01 05:34:39 -0700

    Javascript

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const squaresize = parseInt(argv[2]);
+
 if (isNaN(squaresize)) {
   console.log('Missing size');
-} else {
-  for (let i = 0; i <= squaresize; i++) {
+} 
+else {
+  for (let i = 0; i < squaresize; i++) {
     console.log('X'.repeat(squaresize));
   }
 }
