1292eca06a7e4f7ec7f0733266ae81203c6201e3
MKangabire <m.kangabire@alueducation.com>
2023-10-01 07:05:48 -0700

    Javascript

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const Rectangle = class {
-	  constructor (w, h) {
-		      if (w <= 0 || w === undefined || h <= 0 || h === undefined) { } else {
-			            this.width = w;
-			            this.height = h;
-			          }
-		    }
+  constructor (w, h) {
+    if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
 };
 module.exports = Rectangle;
