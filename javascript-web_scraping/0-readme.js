d97d609a5899168ecfadc94038d61493f6a7f9e9
MKangabire <m.kangabire@alueducation.com>
2023-10-04 02:43:33 -0700

    webscraping

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -6,12 +6,11 @@ if (argv[2]) {
   fs.readFile(argv[2], 'utf-8', (err, data) => {
     if (err) {
       console.error('An error occurred while reading the file:', err);
-      } else {
-          console.log(data);
-          }
-    });
-} 
-else {
+    } else {
+      console.log(data);
+    }
+  });
+} else {
   console.error('Usage: ./script-name.js <file-path>');
   process.exit(1); // Exit with an error code
 }
