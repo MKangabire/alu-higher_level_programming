6e1d5ba3e6c28425b190172354e312fdd5597759
MKangabire <m.kangabire@alueducation.com>
2023-10-04 03:46:40 -0700

    webscraping

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.error('Usage: node get-status-code.js <URL>');
+    process.exit(1); // Exit with an error code
+}
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1); // Exit with an error code
+    }
+
+    console.log(`code: ${response.statusCode}`);
+});
